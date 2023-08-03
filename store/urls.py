be136cf89123a352cb6b9fdca0ae206220170508
erastoff <yuriy.erastov@yandex.ru>
2023-08-03 22:58:53 +0300

    Second Part - Placeholders and Tags

--- store/urls.py
+++ store/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from products.views import index, products
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', products, name='products'),
+    path("admin/", admin.site.urls),
+    path("", index, name="index"),
+    path("products/", products, name="products"),
 ]
