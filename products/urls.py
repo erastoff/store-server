78ac356f22dd8c6b5b2d3569ee5cf877ef2b9a94
erastoff <yuriy.erastov@yandex.ru>
2023-08-20 13:04:44 +0300

    Ninth Part - Template Fragment Caching

--- products/urls.py
+++ products/urls.py
@@ -2,17 +2,13 @@ from django.urls import path
 
 from products.views import ProductsListView, basket_add, basket_remove
 
-from django.views.decorators.cache import (
-    cache_page,
-)  # first way to cache whole page - not recommended
+# from django.views.decorators.cache import (cache_page,)  # first way to cache whole page - not recommended
 
 app_name = "products"
 
 urlpatterns = [
-    path(
-        "", cache_page(30)(ProductsListView.as_view()), name="index"
-    ),  # first way to cache whole page - not recommended
-    # path("", ProductsListView.as_view(), name="index"),
+    # path("", cache_page(30)(ProductsListView.as_view()), name="index"),  # first way to cache whole page - not recommended
+    path("", ProductsListView.as_view(), name="index"),
     path("category/<int:category_id>/", ProductsListView.as_view(), name="category"),
     path("page/<int:page>/", ProductsListView.as_view(), name="paginator"),
     path("baskets/add/<int:product_id>/", basket_add, name="basket_add"),
